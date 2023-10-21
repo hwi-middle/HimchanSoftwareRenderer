489eb9b301e17546300f9e746a056125fc8b1114
Ju Hwijung <gnlwnd5084@gmail.com>
2023-10-21 19:39:37 +0900

    픽셀 찍기, 콘솔창 출력 구현

---
+++ HimchanSoftwareRenderer/HimchanSoftwareRenderer.cpp
@@ -0,0 +1,82 @@
+﻿#include <iostream>
+#include <queue>
+#include <string>
+#include <vector>
+#include <Windows.h>
+
+#ifdef _DEBUG
+#pragma comment(linker,"/entry:WinMainCRTStartup /subsystem:console")
+#endif
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+HINSTANCE g_hInst;
+LPCTSTR lpszClass = TEXT("HimchanRenderer");
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdParam, int nCmdShow)
+{
+	HWND hWnd;
+	MSG Message;
+	WNDCLASS WndClass;
+	g_hInst = hInstance;
+	WndClass.cbClsExtra = 0;
+	WndClass.cbWndExtra = 0;
+	WndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	WndClass.hInstance = hInstance;
+	WndClass.lpfnWndProc = WndProc;
+	WndClass.lpszClassName = lpszClass;
+	WndClass.lpszMenuName = NULL;
+	WndClass.style = CS_HREDRAW | CS_VREDRAW;
+	RegisterClass(&WndClass);
+	hWnd = CreateWindow(lpszClass, lpszClass, WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, 640, 480,
+		NULL, (HMENU)NULL, hInstance, NULL);
+	ShowWindow(hWnd, nCmdShow);
+
+	while (GetMessage(&Message, NULL, 0, 0)) {
+		TranslateMessage(&Message);
+		DispatchMessage(&Message);
+	}
+	return (int)Message.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
+{
+	HDC hdc;
+	PAINTSTRUCT ps;
+
+	switch (iMessage)
+	{
+	case WM_CREATE:
+	{
+
+		return 0;
+	}
+
+	case WM_PAINT:
+	{
+		hdc = BeginPaint(hWnd, &ps);
+		SetPixel(hdc, 10, 10, RGB(255, 0, 0));
+		SetPixel(hdc, 12, 12, RGB(255, 0, 0));
+		EndPaint(hWnd, &ps);
+			break;
+	}
+	case WM_CHAR:
+	{
+		switch (wParam)
+		{
+		case 'r':
+			std::cout << "Hello";
+			break;
+		}
+		return 0;
+	}
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+
+	return (DefWindowProc(hWnd, iMessage, wParam, lParam));
+}
