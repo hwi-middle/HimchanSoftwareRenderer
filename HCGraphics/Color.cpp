319775a321a57973e962c0a7533223840898b848
Ju Hwijung <contact@juhwijung.com>
2024-07-14 12:50:32 +0900

    Color 필드 추가, ToColor32 구현

--- HCGraphics/Color.cpp
+++ HCGraphics/Color.cpp
@@ -1,3 +1,13 @@
 ﻿#include "pch.h"
-
 using namespace HC;
+
+const Color Color::Black(0.f, 0.f, 0.f, 1.f);
+const Color Color::Gray(0.5f, 0.5f, 0.5f, 1.f);
+const Color Color::White(1.f, 1.f, 1.f, 1.f);
+const Color Color::Red(1.f, 0.f, 0.f, 1.f);
+const Color Color::Green(0.f, 1.f, 0.f, 1.f);
+const Color Color::Blue(0.f, 0.f, 1.f, 1.f);
+const Color Color::Yellow(1.f, 0.92f, 0.016f, 1.f);
+const Color Color::Cyan(0.f, 1.f, 1.f, 1.f);
+const Color Color::Magenta(1.f, 0.f, 1.f, 1.f);
+const Color Color::Clear(0.f, 0.f, 0.f, 0.f);
--- HCGraphics/Color.h
+++ HCGraphics/Color.h
@@ -8,13 +8,32 @@ namespace HC
 
 		FORCEINLINE constexpr Color() = default;
 		FORCEINLINE explicit constexpr Color(float InR, float InG, float InB, float InA = 1.f) : R(InR), G(InG), B(InB), A(InA) {}
-		FORCEINLINE explicit constexpr Color(Color32 InColor) : R(InColor.R), G(InColor.G), B(InColor.B), A(InColor.A) {}
-
+		FORCEINLINE explicit constexpr Color(Color32& InColor)
+		{
+			constexpr float OneOver255 = 1.f / 255.f;
+			R = float(InColor.R) * OneOver255;
+			G = float(InColor.G) * OneOver255;
+			B = float(InColor.B) * OneOver255;
+			A = float(InColor.A) * OneOver255;
+		}
 
 		float R = 0.f;
 		float G = 0.f;
 		float B = 0.f;
 		float A = 1.f;
+
+		static const Color Black;
+		static const Color Gray;
+		static const Color White;
+		static const Color Red;
+		static const Color Green;
+		static const Color Blue;
+		static const Color Yellow;
+		static const Color Cyan;
+		static const Color Magenta;
+		static const Color Clear;
+
+		FORCEINLINE constexpr Color32 ToColor32() const;
 
 		FORCEINLINE constexpr Color operator+(const Color& InColor) const;
 		FORCEINLINE constexpr Color operator-(const Color& InColor) const;
@@ -29,6 +48,16 @@ namespace HC
 		FORCEINLINE constexpr bool operator!=(const Color& InColor) const;
 
 	};
+
+	FORCEINLINE constexpr Color32 Color::ToColor32() const
+	{
+		return Color32(
+			(uint8)(0.5f + Math::Clamp(R, 0.f, 1.f) * 255.f),
+			(uint8)(0.5f + Math::Clamp(G, 0.f, 1.f) * 255.f),
+			(uint8)(0.5f + Math::Clamp(B, 0.f, 1.f) * 255.f),
+			(uint8)(0.5f + Math::Clamp(A, 0.f, 1.f) * 255.f)
+		);
+	}
 
 	FORCEINLINE constexpr Color Color::operator+(const Color& InColor) const
 	{
